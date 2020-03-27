1e11beb7e0fa0ce53ad4c8d5f8ac3263cb6e8d22
Richard Ascanio <rascanio97@gmail.com>
2020-03-27 09:19:18 -0400

    final commit

--- app/src/main/java/com/richard/viewbindingexample/MainActivity.java
+++ app/src/main/java/com/richard/viewbindingexample/MainActivity.java
@@ -1,14 +1,36 @@
 package com.richard.viewbindingexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.richard.viewbindingexample.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Clicked button", Toast.LENGTH_SHORT).show();
+            }
+        });
+        binding.button.setText("My button");
+
+        binding.myTextView.setText("View Binding example");
+        binding.myTextView.setTextColor(ContextCompat.getColor(MainActivity.this, R.color.white));
+
+        binding.imageView.setImageResource(R.drawable.ic_launcher_background);
     }
 }
