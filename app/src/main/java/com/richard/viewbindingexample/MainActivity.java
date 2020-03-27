6ea52e2d984760adc465675d7dc67c273102a403
Richard Ascanio <rascanio97@gmail.com>
2020-03-27 09:04:50 -0400

    first commit

---
+++ app/src/main/java/com/richard/viewbindingexample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.richard.viewbindingexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
